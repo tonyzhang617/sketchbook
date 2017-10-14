880a45bc7b35a9d25b483eb1981558890a313488
tonyzhang617 <tonyzhang617@gmail.com>
2017-10-14 13:05:33 -0400

    Make the rectangle draggable

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import { Layer, Rect, Stage, Group } from 'react-konva';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shapes: [],
+    };
+  }
+
+  componentWillMount() {
+    var newShapes = [(<Rect x={10} y={10} width={50} height={50} fill='blue' onDragStart={event => console.log(event)} draggable='true'/>)];
+    this.setState({
+      shapes: newShapes,
+    });
+  }
+
   render() {
-    return (
+    var stage = (
       <Stage width={800} height={800}>
         <Layer>
-          <Rect x={10} y={10} width={50} height={50} fill='blue' />
+          {this.state.shapes}
         </Layer>
       </Stage>
     );
+
+    return stage;
   }
 }
 
