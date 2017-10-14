26ba76ba0aa5579808d86870fef2ef95bc02adda
tonyzhang617 <tonyzhang617@gmail.com>
2017-10-14 18:31:30 -0400

    Implement create rectangles with mouse

--- src/App.js
+++ src/App.js
@@ -7,11 +7,30 @@ class App extends Component {
     this.state = {
       shapes: [],
       isMouseDown: false,
+      newShapeParams: null,
+      newShape: null,
     };
   }
 
+  createNewShape(params) {
+    return (
+      <Rect
+        key={params.key}
+        x={params.x}
+        y={params.y}
+        width={params.width}
+        height={params.height}
+        fill='green'
+      />
+    );
+  }
+
   componentWillMount() {
-    var newShapes = [(<Rect x={10} y={10} width={50} height={50} fill='blue' onDragStart={event => console.log(event)} draggable='true'/>)];
+    var newShapes = [
+      <Rect x={10} y={10} key={0}
+        width={50} height={50}
+        fill='blue' />
+    ];
     this.setState({
       shapes: newShapes,
     });
@@ -19,31 +38,58 @@ class App extends Component {
 
   componentDidMount() {
     var stage = this.refs.stage.getStage();
-    
+
     stage.on('contentMousedown', ({evt}) => {
+      var params = {
+        key: this.state.shapes[this.state.shapes.length-1].key + 1,
+        x: evt.clientX,
+        y: evt.clientY,
+        width: 1,
+        height: 1,
+      };
+
       this.setState({
+        newShape: this.createNewShape(params),
+        newShapeParams: params,
         isMouseDown: true,
       });
       console.log('Mouse Down');
     });
+    stage.on('contentMousemove', ({evt}) => {
+      if (this.state.isMouseDown) {
+        var params = { ...this.state.newShapeParams };
+        params.width = evt.clientX - params.x;
+        params.height = evt.clientY - params.y;
+
+        this.setState({
+          newShape: this.createNewShape(params),
+          newShapeParams: params,
+        });
+
+        console.log(evt.clientX, evt.clientY);
+      }
+    });
     stage.on('contentMouseup', ({evt}) => {
+      var params = { ...this.state.newShapeParams };
+      params.width = evt.clientX - params.x;
+      params.height = evt.clientY - params.y;
+
       this.setState({
+        newShape: null,
+        newShapeParams: null,
+        shapes: this.state.shapes.concat([this.createNewShape(params)]),
         isMouseDown: false,
       });
       console.log('Mouse Up');
-    });
-    stage.on('contentMousemove', ({evt}) => {
-      if (this.state.isMouseDown) {
-        console.log(evt.clientX, evt.clientY);
-      }
     });
   }
 
   render() {
     var stage = (
-      <Stage ref='stage' width={800} height={800}>
+      <Stage ref='stage' width={640} height={480}>
         <Layer>
-          {this.state.shapes}
+          { this.state.shapes }
+          { this.state.newShape }
         </Layer>
       </Stage>
     );
